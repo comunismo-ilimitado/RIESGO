08be38baef5156097c13ea3753480e4910b8a656
neerajpreetkaur <45363591+neerajpreetkaur@users.noreply.github.com>
2019-02-26 08:58:13 -0600

    Create ReinforcementController.java

---
+++ src/ReinforcementController.java
@@ -0,0 +1,61 @@
+import java.awt.event.ActionEvent;
+import java.util.*;
+import javax.swing.*;
+
+ class ReinforcementController {
+	
+	 
+		public List<Country> getMyCountries(Player player){
+			List<Country> countries=new ArrayList<Country>();
+			for(Map.Entry<String, Country> entry:ReadingFiles.CountryNameObject.entrySet()){       
+				if(entry.getValue().getOwner().equals(player))
+				{
+					countries.add(entry.getValue());
+				}
+				else
+					continue;
+			}
+			return countries;
+		}
+		
+	
+	
+	
+	
+
+	public void calculateReinforcementArmies(Player player)
+	{
+		
+		int totalCountriesOFPlayer=player.getTotalCountriesOccupied().size();
+		int total_armies_to_reinforce; 
+		total_armies_to_reinforce = totalCountriesOFPlayer/3;	
+		
+		if(totalCountriesOFPlayer<3)
+		{
+			player.setPlayerTotalArmiesNotDeployed(3);
+		}
+		else
+		{
+			player.setPlayerTotalArmiesNotDeployed(total_armies_to_reinforce);
+		}
+		
+		
+	}
+	
+	public void updateValue(Player player, Country country )
+	{
+		country.setNoOfArmies(country.getNoOfArmies()+1);
+	}
+	
+	
+	
+	
+
+
+ 
+
+	
+	
+}
+ 
+

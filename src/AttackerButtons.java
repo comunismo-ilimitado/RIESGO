7dab389f49d1f55882be434c412ee50b3a1405bd
bhargavur <bhargavur29@gmail.com>
2019-02-27 17:24:37 -0500

    Add files via upload

--- src/AttackerButtons.java
+++ src/AttackerButtons.java
@@ -1,22 +1,23 @@
+package view;
 import java.util.*;
+
+import controller.AttackController;
+import model.Country;
+import model.Player;
+
 import java.io.*;
-/**
- * 
- * @author Komal
- * @version 1.1
- */
 public class AttackerButtons {
 	AttackController attackController = new AttackController();
 	public void endReinforcementsPhaseButton(Player player){
-		
 		//call getMyCountries(player) method and display countries
 	}
 	public void endAttackPhaseButton(Player player) {
 		//call fortification controller
 	}
-	public void attackButton(Country attacker, Country defender) {
-		if(attacker.getNoOfArmies()>=2 && defender.getNoOfArmies()!=0)
+	public String attackButton(Country attacker, Country defender) {
+		if(attacker.getNoOfArmies()>=2 && defender.getNoOfArmies()>=2)
 		{
+			String answer = "";
 			//display the number of attacker dice
 			int attackerDice=attackController.setNoOfDice(attacker, "A");
 			//display the number of defender dice
@@ -35,9 +36,11 @@ public class AttackerButtons {
 				int defenderMax=getMaxValue(defenderDiceRoll);
 				if(attackerMax<=defenderMax) {
 					attackController.updateArmies(attacker);
+					answer= "Defender Won";
 				}
 				else {
 					attackController.updateArmies(defender);
+					answer= "You Won";
 				}
 				attackerDiceRoll.remove(attackerDiceRoll.indexOf(attackerMax));
 				defenderDiceRoll.remove(defenderDiceRoll.indexOf(defenderMax));
@@ -48,17 +51,23 @@ public class AttackerButtons {
 			}
 			if(defender.getNoOfArmies()==0) {
 				attackController.updateOwner(defender, attacker.getOwner());
+				answer = answer+"and you occupied this country.";
 				//call place armies method from reinforcements phase and force player to place armies
 				//that are at least the number of dice rolled by attacker
 			}
 			if(attackController.getMyCountries(defender.getOwner()).size()==0) {
 				//add code to give all defenders cards to attacker
 			}
+			return answer;
 		}
 		else
 		{
-			//tell the player that you must have at least 2 armies in the country to attack
-			//and show his countries again
+			if(attacker.getNoOfArmies()<=1)
+			return "Your country must have more than one army";
+			else if(defender.getNoOfArmies()<=1)
+				return "Please a country with more than one army to attack";
+			else
+				return "Wrong input";
 		}
 	}
 	public int getMaxValue(List<Integer> list) {

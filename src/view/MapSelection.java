43a35a5c8ce7f932f952f69002f9aec724a6f6b3
greeshmasunil10 <47255206+greeshmasunil10@users.noreply.github.com>
2019-03-01 17:02:12 -0500

    Add files via upload

---
+++ src/view/MapSelection.java
@@ -0,0 +1,110 @@
+import java.util.List;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+
+import javax.swing.*;
+
+public class MapSelection {
+
+	private static JFrame MapFrame;
+	private JLabel HeaderLabel;
+	private JLabel HeaderLabel2;
+	private static JButton SelectButton;
+	private static JComboBox MapCombobox;
+	private static String MapSelected;
+
+	static List<String> MapFiles = new ArrayList<String>();
+
+	public static void main(String args[]) {
+		MapSelection screen1 = new MapSelection();
+		// Use following method to add Maps to the Displayed List
+		screen1.addMap("World1.map");
+		screen1.addMap("World2.txt");
+		screen1.addMap("World3.txt");
+		screen1.addMap("World4.txt");
+		screen1.addMap("World5.txt");
+		screen1.setUpScreen();
+		String temp = screen1.getSelectedMap();
+
+	}
+
+	/**
+	 * Setting up frame components
+	 */
+	public MapSelection() {
+		// MapSelection screen1= new MapSelection();
+		// Use following method to add Maps to the Displayed List
+		this.addMap("World1.map");
+		this.addMap("World2.txt");
+		this.addMap("World3.txt");
+		this.addMap("World4.txt");
+		this.addMap("World5.txt");
+		this.setUpScreen();
+		String temp = this.getSelectedMap();
+	}
+
+	public void setUpScreen() {
+		MapFrame = new JFrame("Start-up phase");
+		MapFrame.setSize(500, 500);
+
+		HeaderLabel = new JLabel("Choose the map");
+		HeaderLabel.setBounds(120, 100, 150, 50);
+
+		HeaderLabel2 = new JLabel("STEP 1: MAP SELECTION ");
+		HeaderLabel2.setBounds(90, 40, 150, 50);
+
+		String Maps[] = MapFiles.toArray(new String[0]);
+		MapCombobox = new JComboBox(Maps);
+		MapCombobox.setBounds(120, 150, 200, 20);
+
+		SelectButton = new JButton("OK");
+		SelectButton.setBounds(175, 200, 90, 20);
+
+		MapFrame.add(HeaderLabel);
+		MapFrame.add(HeaderLabel2);
+		MapFrame.add(MapCombobox);
+		MapFrame.add(SelectButton);
+		HeaderLabel.setVisible(true);
+		HeaderLabel2.setVisible(true);
+		MapCombobox.setVisible(true);
+		SelectButton.setVisible(true);
+
+		MapFrame.setLayout(null);
+		MapFrame.setVisible(true);
+
+	}
+
+	/**
+	 * 
+	 * @param item
+	 */
+	public static void addMap(String item) {
+		MapFiles.add(item);
+	}
+
+	/**
+	 * This method returns the user input i.e. the map selected This is the action
+	 * performed for SelectButton
+	 * 
+	 * @return MapSelected
+	 */
+	public static String getSelectedMap() {
+
+		SelectButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				MapSelected = (String) MapCombobox.getSelectedItem();
+				System.out.print(MapSelected);
+				// invoke next screen here i.e Load Maps
+				MapFrame.dispose();
+				LoadSelectedMap.loadMap();
+				// jumps to new Window
+			}
+		});
+		return MapSelected;
+
+	}
+
+}
---
+++ src/view/StartUpWindow.java
@@ -0,0 +1,84 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+/**
+ * First Paga of Start up phase
+ * Start here
+ *
+ *
+ */
+public class StartUpWindow {
+
+	private static JFrame window;
+	private JLabel HeaderLabel;
+	private JButton select1, select2,select3;
+
+	public StartUpWindow() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+      StartUpWindow temp= new StartUpWindow();
+      temp.setup();    
+	}
+
+	 public void setup() {
+		window = new JFrame("Start-up phase");
+		window.setSize(500, 700);
+
+		HeaderLabel = new JLabel("Choose the map");
+		HeaderLabel.setBounds(120, 100, 150, 50);
+
+		select1 = new JButton("Select from List");
+		select1.setBounds(100, 200, 200, 50);
+
+		select2 = new JButton("Load Map");
+		select2.setBounds(100, 300, 200, 50);
+		
+		select3 = new JButton("Create New Map");
+		select3.setBounds(100, 400, 200, 50);
+
+		window.add(HeaderLabel);
+		window.add(select1);
+		window.add(select2);
+		window.add(select3);
+
+		HeaderLabel.setVisible(true);
+		
+		select1.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				window.dispose();
+				System.out.print("Select fromlist");
+ 				MapSelection Map= new MapSelection();
+			}});
+		select2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				window.dispose();
+				BrowseMapFile Map= new BrowseMapFile();
+			}});
+
+		select3.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				window.dispose();
+				CreateMap CreatedMap= new CreateMap();
+			}});
+		
+		select1.setVisible(true);
+		select2.setVisible(true);
+		select3.setVisible(true);
+
+
+		window.setLayout(null);
+		window.setVisible(true);
+	}
+
+}

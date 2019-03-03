001f692c040760aebaae266a442401ad30c2f35d
greeshmasunil10 <47255206+greeshmasunil10@users.noreply.github.com>
2019-03-02 23:50:28 -0500

    Add files via upload

--- src/controller/SaveCreatedMap.java
+++ src/controller/SaveCreatedMap.java
@@ -16,86 +16,81 @@ import view.CreateMap;
 public class SaveCreatedMap {
 
 	BufferedWriter bw;
+
 	public SaveCreatedMap() {
-		File f1= new File("Resources/UserMap.txt");
+		File f1 = new File("Resources/UserMap.txt");
 		try {
-			if(f1.createNewFile())
+			if (f1.createNewFile())
 				System.out.println("File created");
-			else 
+			else
 				System.out.println("File already exists");
 			FileOutputStream fos = new FileOutputStream(f1);
-			 bw = new BufferedWriter(new OutputStreamWriter(fos));
+			bw = new BufferedWriter(new OutputStreamWriter(fos));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		Init();
-		
-	}
-	
-	public void WriteIntoFile(String s) {
-		try {
-			
-			bw.write(s);bw.newLine();
-			
-			}catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
-    public void Init() {
-		
-		List<Continent> Continents=new ArrayList<>();
-		Continents=CreateMap.ContinentsObjectList;
+
+	public void WriteIntoFile(String s) {
 		try {
-		WriteIntoFile("[Map]");
-		WriteIntoFile("Image=UserMap.bmp");
-		WriteIntoFile("");
-		WriteIntoFile("wrap=yes");
-		WriteIntoFile("scroll=none");
-		WriteIntoFile("warn=yes");
-		bw.newLine();
-		WriteIntoFile("[Continents]");
-		for(Continent in:CreateMap.ContinentsObjectList ) {
-		WriteIntoFile(in.getName()+"="+in.getControlValue());
+
+			bw.write(s);
+			bw.newLine();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		bw.newLine();
-		WriteIntoFile("[Territories]");
-		
-		Random rand1 = new Random();
-		Random rand2 = new Random();
-		
-		int i=1;
-		for(Continent cc: CreateMap.ContinentsObjectList) {
-		 for(Country cn: cc.getCountries()) {
-			int value1 = rand1.nextInt(255);
-			int value2 = rand2.nextInt(255);
-			bw.write(i+"," +cn.getName()+ "," + value1 + "," + value2 +","+ cc.getName() );
-			
-			for(Country cin: cn.getNeighbors()) {
-				bw.write("," + cn.getName());
+	}
+
+	public void Init() {
+		try {
+			WriteIntoFile("[Map]");
+			WriteIntoFile("Image=UserMap.bmp");
+			WriteIntoFile("");
+			WriteIntoFile("wrap=yes");
+			WriteIntoFile("scroll=none");
+			WriteIntoFile("warn=yes");
+			bw.newLine();
+			WriteIntoFile("[Continents]");
+			for (Continent in : CreateMap.ContinentsObjectList) {
+				WriteIntoFile(in.getName() + "=" + in.getControlValue());
 			}
 			bw.newLine();
-			i++;
-			 }
-			bw.newLine();
-		}
+			WriteIntoFile("[Territories]");
+
+			Random rand1 = new Random();
+			Random rand2 = new Random();
+
+			int i = 1;
+			for (Continent cc : CreateMap.ContinentsObjectList) {
+				for (Country cn : cc.getCountries()) {
+					int value1 = rand1.nextInt(255);
+					int value2 = rand2.nextInt(255);
+					bw.write(i + "," + cn.getName() + "," + value1 + "," + value2 + "," + cc.getName());
+
+					for (Country cin : cn.getNeighbors()) {
+						bw.write("," + cn.getName());
+					}
+					bw.newLine();
+					i++;
+				}
+				bw.newLine();
+			}
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-try {
+		try {
 			bw.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
 
 }

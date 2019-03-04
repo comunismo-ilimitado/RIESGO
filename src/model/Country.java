a61a3d4324366cd31b6d9d37d65752491016e26c
Navjot08 <navjotkb08@gmail.com>
2019-03-03 22:31:00 -0500

    Changed variable name convention

--- src/model/Country.java
+++ src/model/Country.java
@@ -10,92 +10,92 @@ import java.util.*;
 public class Country {
 
 	/**
-	 * CountryId This contains unique id of the country of type integer
+	 * country_id This contains unique id of the country of type integer
 	 */
-	private int CountryId;
+	private int country_id;
 
-	// Continent which it belongs to
-	private Continent Continent;
+	// continent which it belongs to
+	private Continent continent;
 
 	/**
-	 * ContinentId This contains unique id of the continent of type integer
+	 * continent_id This contains unique id of the continent of type integer
 	 */
-	private int ContinentId;
+	private int continent_id;
 
 	/**
-	 * OwnedPlayer This contains name of the player who owns this country of type
+	 * owned_player This contains name of the player who owns this country of type
 	 * Player
 	 */
-	private Player OwnedPlayer;
+	private Player owned_player;
 
 	/**
-	 * Name This is the name of the country of type String
+	 * name This is the name of the country of type String
 	 */
-	private String Name;
+	private String name;
 
 	/**
-	 * Neighbors This is the list of the neighboring countries of this country
+	 * neighbors This is the list of the neighboring countries of this country
 	 */
-	private List<Country> Neighbors;
+	private List<Country> neighbors;
 
 	/**
-	 * NoOfArmies This contains the number of armies this country has
+	 * no_of_armies This contains the number of armies this country has
 	 */
-	private int NoOfArmies;
+	private int no_of_armies;
 
 	/**
 	 * Get the id of the country
 	 * 
-	 * @return CountryId Id of the country of type Integer
+	 * @return country_id Id of the country of type Integer
 	 */
 	public int getCountryId() {
-		return CountryId;
+		return country_id;
 	}
 
 	/**
 	 * Set the id of the country
 	 * 
-	 * @param CountryId Id of the country of type Integer
+	 * @param country_id Id of the country of type Integer
 	 */
-	public void setCountryId(int CountryId) {
-		this.CountryId = CountryId;
+	public void setCountryId(int country_id) {
+		this.country_id = country_id;
 	}
 
-	// Gets Continent ID
+	// Gets continent ID
 	public Continent getContinent() {
-		return Continent;
+		return continent;
 	}
 
-	// Set Continent ID
-	public void setContinent(Continent Continent) {
-		this.Continent = Continent;
+	// Set continent ID
+	public void setContinent(Continent continent) {
+		this.continent = continent;
 	}
 
 	/**
 	 * Get the id of the continent
 	 * 
-	 * @return ContinentId Id of the continent of type Integer
+	 * @return continent_id Id of the continent of type Integer
 	 */
 	public int getContinentId() {
-		return ContinentId;
+		return continent_id;
 	}
 
 	/**
 	 * Set the id of the continent
 	 * 
-	 * @param ContinentId Id of the continent of type Integer
+	 * @param continent_id Id of the continent of type Integer
 	 */
-	public void setContinentId(int ContinentId) {
-		this.ContinentId = ContinentId;
+	public void setContinentId(int continent_id) {
+		this.continent_id = continent_id;
 	}
 
 	/**
 	 * Get the owner of the country
 	 * 
-	 * @return OwnedPlayer It returns the object of the Player type
+	 * @return owned_player It returns the object of the Player type
 	 */
 	public Player getOwner() {
-		return OwnedPlayer;
+		return owned_player;
 	}
 
 	/**
@@ -103,44 +103,44 @@ public class Country {
 	 * 
 	 * @param Player It is the object of the Player type
 	 */
-	public void setPlayer(Player Player) {
-		this.OwnedPlayer = Player;
+	public void setPlayer(Player player) {
+		this.owned_player = player;
 	}
 
 	/**
 	 * Get the name of the country
 	 * 
-	 * @return Name It is the name of the country of type String
+	 * @return name It is the name of the country of type String
 	 */
 	public String getName() {
-		return Name;
+		return name;
 	}
 
 	/**
 	 * Set the name of the country
 	 * 
-	 * @param Name It is the name of the country of type String
+	 * @param name It is the name of the country of type String
 	 */
-	public void setName(String Name) {
-		this.Name = Name;
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	/**
 	 * Get the list of the neighbor countries
 	 * 
-	 * @return Neighbors It returns the list of the neighbor countries
+	 * @return neighbors It returns the list of the neighbor countries
 	 */
 	public List<Country> getNeighbors() {
-		return Neighbors;
+		return neighbors;
 	}
 
 	/**
 	 * Set the neighbors list of this country
 	 * 
-	 * @param Neighbors It takes the list of the neighbor countries
+	 * @param neighbors It takes the list of the neighbor countries
 	 */
-	public void setNeighbors(List<Country> Neighbors) {
-		this.Neighbors = Neighbors;
+	public void setNeighbors(List<Country> neighbors) {
+		this.neighbors = neighbors;
 	}
 
 	/**
@@ -148,26 +148,26 @@ public class Country {
 	 * 
 	 * @param Neighbor It takes the object of type Country
 	 */
-	public void addNeighbors(Country Neighbor) {
-		this.Neighbors.add(Neighbor);
+	public void addNeighbors(Country neighbor) {
+		this.neighbors.add(neighbor);
 	}
 
 	/**
 	 * This method is used to get the number of armies in this country
 	 * 
-	 * @return NoOfArmies It is of type Integer
+	 * @return no_of_armies It is of type Integer
 	 */
 	public int getNoOfArmies() {
-		return NoOfArmies;
+		return no_of_armies;
 	}
 
 	/**
 	 * This method is used to set the number of armies in this country
 	 * 
-	 * @param NoOfArmies It is of type Integer
+	 * @param no_of_armies It is of type Integer
 	 */
-	public void setNoOfArmies(int NoOfArmies) {
-		this.NoOfArmies = NoOfArmies;
+	public void setNoOfArmies(int no_of_armies) {
+		this.no_of_armies = no_of_armies;
 	}
 
 	/**
@@ -176,10 +176,10 @@ public class Country {
 	 * @param name It takes the name of the country of type String
 	 */
 	public Country(String name) {
-		this.NoOfArmies = 0;
-		this.Neighbors = new ArrayList<>();
-		this.Name = name;
-		this.OwnedPlayer = new Player(0);
+		this.no_of_armies = 0;
+		this.neighbors = new ArrayList<>();
+		this.name = name;
+		this.owned_player = new Player(0);
 	}
 
 }

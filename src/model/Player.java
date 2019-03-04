025a9a188ddbec23abd57e941cbc34dda647c092
Navjot08 <navjotkb08@gmail.com>
2019-03-03 22:42:32 -0500

    updated

--- src/model/Player.java
+++ src/model/Player.java
@@ -5,122 +5,122 @@ import java.util.*;
 //Represents a player in the game
 public class Player {
 	
-		//Player Name
-		private String Name;
+		//Player name
+		private String name;
 		
 		//Player ID
-		private int PlayerId;
+		private int player_id;
 		
 		//Total number of armies a player has
-		private int TotalArmies;
+		private int total_armies;
 		
 		//Armies that are not assigned to any country
-		private int TotalArmiesNotDeployed;
+		private int total_armies_not_deployed;
 		
 		//Total number of armies a player owns
-		private List<Country> TotalCountriesOccupied;
+		private List<Country> total_countries_occupied;
 		
 		
-		private List<Country> CountriesOccupied;
+		private List<Country> countries_occupied;
 		
 		//Number of Continents occupied
-		private List<Continent> ContinentsOccupied;
+		private List<Continent> continents_occupied;
 		
 		//The cards that a player holds
-		private List<CardTypes> Cards;
+		private List<CardTypes> cards;
 		
 		//Color to represent player owned countries
-		private Color MyColor;
+		private Color my_color;
 		
 		
 		//Default Constructor
-		public Player(int playerId) {
-			this.PlayerId = PlayerId;
-			this.TotalCountriesOccupied = new ArrayList<>();
-			this.CountriesOccupied = new ArrayList<>();
-			this.Cards = new ArrayList<>();
+		public Player(int player_id) {
+			this.player_id = player_id;
+			this.total_countries_occupied = new ArrayList<>();
+			this.countries_occupied = new ArrayList<>();
+			this.cards = new ArrayList<>();
 		}
 	
-		//Gets PlayerID
+		//Gets player_id
 		public int getPlayerId() {
-			return PlayerId;
+			return player_id;
 		}
 	
-		//Sets PlayerID
-		public void setPlayerId(int PlayerId) {
-			this.PlayerId = PlayerId;
+		//Sets player_id
+		public void setPlayerId(int player_id) {
+			this.player_id = player_id;
 		}
 
 	
-		//Gets Player Name
+		//Gets Player name
 		public String getPlayerName() {
-			return Name;
+			return name;
 		}
 
-		//Sets Player Name
-		public void setPlayerName(String Name) {
-			this.Name = Name;
+		//Sets Player name
+		public void setPlayerName(String name) {
+			this.name = name;
 		}
 		
 		//Gets Player Armies
 		public int getPlayerArmies() {
-			return TotalArmies;
+			return total_armies;
 		}
 
 		//Sets Player Armies
-		public void setPlayerArmies(int TotalArmies) {
-			this.TotalArmies = TotalArmies;
+		public void setPlayerArmies(int total_armies) {
+			this.total_armies = total_armies;
 		}
 		
 		//Gets Player Armies that are not deployed
 		public int getPlayerArmiesNotDeployed() {
-			return TotalArmiesNotDeployed;
+			return total_armies_not_deployed;
 		}
 
 		//Sets Player Armies that are not deployed
-		public void setPlayerTotalArmiesNotDeployed(int TotalArmiesNotDeployed) {
-			this.TotalArmiesNotDeployed = TotalArmiesNotDeployed;
+		public void setPlayerTotalArmiesNotDeployed(int total_armies_not_deployed) {
+			this.total_armies_not_deployed = total_armies_not_deployed;
 		}
 				
 		//Gets Countries Occupied
 		public List<Country> getTotalCountriesOccupied() {
-			return TotalCountriesOccupied;
+			return total_countries_occupied;
 		}
 
 		//Sets Countries Occupied
-		public void setTotalCountriesOccupied(List<Country> TotalCountriesOccupied) {
-			this.TotalCountriesOccupied = TotalCountriesOccupied;
+		public void setTotalCountriesOccupied(List<Country> total_countries_occupied) {
+			this.total_countries_occupied = total_countries_occupied;
 		}
-		public void addCountriesOccupied(Country countrieOccupied) {
-			this.TotalCountriesOccupied.add(countrieOccupied);
+		public void addCountriesOccupied(Country countriesOccupied) {
+			this.total_countries_occupied.add(countriesOccupied);
 		}
 
 		
 		//Gets Continents Occupied
 		public List<Continent> getContinentsOccupied() {
-			return ContinentsOccupied;
+			return continents_occupied;
 		}
 		//Sets Continents Occupied
-		public void setContinentsOccupied(List<Continent> ContinentsOccupied) {
-			this.ContinentsOccupied = ContinentsOccupied;
+		public void setContinentsOccupied(List<Continent> continents_occupied) {
+			this.continents_occupied = continents_occupied;
 		}
-		//Gets Player Cards
+		//Gets Player cards
 		public List<CardTypes> getPlayerCards() {
-			return Cards;
+			return cards;
 		}
 
-		//Sets Player Cards
-		public void setPlayerCards(List<CardTypes> PlayerCards) {
-			this.Cards = PlayerCards;
+		//Sets Player cards
+		public void setPlayerCards(List<CardTypes> Playercards) {
+			this.cards = Playercards;
 		}
 		
 		//Gets player color
 		public Color getPlayerColor() {
-			return MyColor;
+			return my_color;
 		}
 			
 		//Sets player color
 		public void setPlayerColor(Color color) {
-			this.MyColor = color;
+			this.my_color = color;
 		}			
 }

42cf8b60f00cd37d5109b019b53b93a9322d9c40
bhargavur <bhargavur29@gmail.com>
2019-03-03 22:57:48 -0500

    V 1.2

--- src/controller/ReadingFiles.java
+++ src/controller/ReadingFiles.java
@@ -9,132 +9,123 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
-
 import model.Continent;
 import model.Country;
 import model.Player;
 import view.AssignCountries;
 
 public class ReadingFiles {
-	// public static HashMap<String, Player> playerObject;
-	public static HashMap<Integer, Player> playerId;
-	public static List<Integer> players;
-	public static List<String> CountriesNames, ContinentNames; // List of countries Strings
+	public static HashMap<Integer, Player> PlayerId;
+	public static List<Integer> Players;
+	public static List<String> CountriesNames, ContinentNames;
 	public static HashMap<String, Country> CountryNameObject;
-	public static HashMap<String, Continent> ContinentNameObject; // HashMAp COntaining Country name as // // key and
+	public static HashMap<String, Continent> ContinentNameObject;
 	public static String address = "Resources/World.map";
 	public static String image = "noimage.bmp";
 
-	// returns country object
-
 	public static void Reads() {
 		try {
-		CountryNameObject = new HashMap<>();
-		CountriesNames = new ArrayList<>();
-		ContinentNames = new ArrayList<>();
-		ContinentNameObject = new HashMap<>();
-		// Reading Country File
-		FileReader file = new FileReader(address);
-		BufferedReader bufferedReader = new BufferedReader(file);
-		String temp = "";
-		StringBuffer buffer = new StringBuffer();
-		while ((temp = bufferedReader.readLine()) != null) {
-			buffer.append(temp + "\n");
-		}
-		bufferedReader.close();
-		String string = "\\[.*]";
-		String[] aaa = buffer.toString().trim().replaceAll("\n+", "\n").split(string);
-		String InfoString = aaa[1].trim();
-		String[] findInfo = InfoString.split("\n");
-		for (int i = 0; i < InfoString.length(); i++) {
-			String[] temp1 = findInfo[i].split("=");
-			if (temp1[0].trim().equals("image")) {
-				image = temp1[1].trim();
-				break;
+			CountryNameObject = new HashMap<>();
+			CountriesNames = new ArrayList<>();
+			ContinentNames = new ArrayList<>();
+			ContinentNameObject = new HashMap<>();
+			FileReader file = new FileReader(address);
+			BufferedReader bufferedreader = new BufferedReader(file);
+			String temp = "";
+			StringBuffer buffer = new StringBuffer();
+			while ((temp = bufferedreader.readLine()) != null) {
+				buffer.append(temp + "\n");
 			}
-		}
-
-		String ContinentsString = aaa[2].trim();
-		String CountriesString = aaa[3].trim();
-		String[] tempContinentArray = ContinentsString.split("\n");
-
-		for (int i = 0; i < tempContinentArray.length; i++) {
-			String temporary = tempContinentArray[i].split("=")[0].trim().toLowerCase();
-			int value = Integer.parseInt(tempContinentArray[i].split("=")[1].trim());
-			ContinentNames.add(temporary);
-			ContinentNameObject.put(temporary, new Continent(value, temporary));
-		}
-		
-		String[] tempCountryArray = CountriesString.split("\n");
-		for (int i = 0; i < tempCountryArray.length; i++) {
-			String a = tempCountryArray[i].split(",")[0].trim();
-			CountriesNames.add(a);
-			CountryNameObject.put(a, new Country(a));
-		}
-		for (int i = 0; i < tempCountryArray.length; i++) {
-			String[] a = tempCountryArray[i].split(",");
-			Country temp1 = CountryNameObject.get(a[0].trim());
-			Continent temp2 = ContinentNameObject.get(a[3].trim().toLowerCase());
-			temp1.setContinent(temp2);
-			temp2.addCountrie(temp1);
-			for (int j = 4; j < a.length; j++) {
-				temp1.addNeighbors(CountryNameObject.get(a[j].trim()));
-			}
-		}
-
-		List<String> CountriesNames2 = new ArrayList<String>(CountryNameObject.keySet());
-		Collections.shuffle(CountriesNames2);
-
-		// playerObject = new HashMap<>();
-		players = new ArrayList<>();
-		playerId = new HashMap<>();
-		List<Color> arrayListc = new ArrayList<>();
-		arrayListc.add(Color.cyan);
-		arrayListc.add(Color.GREEN);
-		arrayListc.add(Color.YELLOW);
-		arrayListc.add(Color.ORANGE);
-		arrayListc.add(Color.decode("#33cccc"));
-		arrayListc.add(Color.PINK);
-		
-		
-		int noofplayers = AssignCountries.NumberOfPlayers;
-
-		for (int i = 0; i < noofplayers; i++) {
-			Player player = new Player(i);
-			player.setPlayerColor(arrayListc.get(i));
-			playerId.put(i, player);
-			players.add(i);
-		}
-		try {
-			int n = noofplayers;
-			for (int i = 0; i < CountriesNames2.size(); i++) {
-				for (int j = 0; j < noofplayers; j++) {
-					Country temp1 = CountryNameObject.get(CountriesNames2.get(i + j));
-					Player tempPlayer = playerId.get(j);
-					temp1.setPlayer(tempPlayer);
-					tempPlayer.addCountriesOccupied(temp1);
+			bufferedreader.close();
+			String string = "\\[.*]";
+			String[] dividedstring = buffer.toString().trim().replaceAll("\n+", "\n").split(string);
+			String infostring = dividedstring[1].trim();
+			String[] findinfo = infostring.split("\n");
+			for (int i = 0; i < infostring.length(); i++) {
+				String[] temparray = findinfo[i].split("=");
+				if (temparray[0].trim().equals("image")) {
+					image = temparray[1].trim();
+					break;
 				}
-				i = i + n - 1;
+			}
+
+			String continentsstring = dividedstring[2].trim();
+			String countriesstring = dividedstring[3].trim();
+			String[] tempcontinentarray = continentsstring.split("\n");
+
+			for (int i = 0; i < tempcontinentarray.length; i++) {
+				String temporary = tempcontinentarray[i].split("=")[0].trim().toLowerCase();
+				int value = Integer.parseInt(tempcontinentarray[i].split("=")[1].trim());
+				ContinentNames.add(temporary);
+				ContinentNameObject.put(temporary, new Continent(value, temporary));
+			}
+
+			String[] tempcountryarray = countriesstring.split("\n");
+			for (int i = 0; i < tempcountryarray.length; i++) {
+				String a = tempcountryarray[i].split(",")[0].trim();
+				CountriesNames.add(a);
+				CountryNameObject.put(a, new Country(a));
+			}
+			for (int i = 0; i < tempcountryarray.length; i++) {
+				String[] a = tempcountryarray[i].split(",");
+				Country tempcountry = CountryNameObject.get(a[0].trim());
+				Continent tempcontinent = ContinentNameObject.get(a[3].trim().toLowerCase());
+				tempcountry.setContinent(tempcontinent);
+				tempcontinent.addCountrie(tempcountry);
+				for (int j = 4; j < a.length; j++) {
+					tempcountry.addNeighbors(CountryNameObject.get(a[j].trim()));
+				}
+			}
+
+			List<String> countriesnameslist = new ArrayList<String>(CountryNameObject.keySet());
+			Collections.shuffle(countriesnameslist);
+			Players = new ArrayList<>();
+			PlayerId = new HashMap<>();
+			List<Color> colorslist = new ArrayList<>();
+			colorslist.add(Color.cyan);
+			colorslist.add(Color.GREEN);
+			colorslist.add(Color.YELLOW);
+			colorslist.add(Color.ORANGE);
+			colorslist.add(Color.decode("#33cccc"));
+			colorslist.add(Color.PINK);
+
+			int noofplayers = AssignCountries.NumberOfPlayers;
+
+			for (int i = 0; i < noofplayers; i++) {
+				Player player = new Player(i);
+				player.setPlayerColor(colorslist.get(i));
+				PlayerId.put(i, player);
+				Players.add(i);
+			}
+			try {
+				int n = noofplayers;
+				for (int i = 0; i < countriesnameslist.size(); i++) {
+					for (int j = 0; j < noofplayers; j++) {
+						Country tempcountry = CountryNameObject.get(countriesnameslist.get(i + j));
+						Player tempplayer = PlayerId.get(j);
+						tempcountry.setPlayer(tempplayer);
+						tempplayer.addCountriesOccupied(tempcountry);
+					}
+					i = i + n - 1;
+				}
+			} catch (Exception e) {
+			}
+			int armiesperplayer = 50 - (5 * noofplayers);
+			for (int i = 0; i < noofplayers; i++) {
+				Player currentplayer = PlayerId.get(Players.get(i));
+				List<Country> clist = currentplayer.getTotalCountriesOccupied();
+				int playersize = clist.size();
+				int count = 0;
+				for (int j = 0; j < armiesperplayer; j++) {
+					if (count >= playersize)
+						count = 0;
+					clist.get(count).setNoOfArmies(clist.get(count).getNoOfArmies() + 1);
+					count++;
+
+				}
+
 			}
 		} catch (Exception e) {
-		}
-		int ArmiesPerPlayer = 50 - (5 * noofplayers);
-		for (int i = 0; i < noofplayers; i++) {
-			Player temp1 = playerId.get(players.get(i));
-			List<Country> clis = temp1.getTotalCountriesOccupied();
-			int playersize=clis.size();
-			int count=0;
-			for (int j = 0; j < ArmiesPerPlayer; j++) {
-				if(count>=playersize)
-					count=0;
-				clis.get(count).setNoOfArmies(clis.get(count).getNoOfArmies()+1);
-				count++;
-
-			}
-
-		}
-		}
-		catch(Exception e) {
 			System.out.println("ERROR IN MAP");
 		}
 	}

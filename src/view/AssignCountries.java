f23fffc023fee220194c489571b2a9ceae23725c
Pazim Goyal <pazimgoyal@gmail.com>
2019-03-05 01:08:34 -0500

    update

--- src/view/AssignCountries.java
+++ src/view/AssignCountries.java
@@ -12,14 +12,14 @@ public class AssignCountries {
 
 	private static JComboBox PNumberJCombo;
 	private static JButton OKButton;
-//	static String NumberOfPlayers="6";
 	private static JFrame Frame1;
-	public static int NumberOfPlayers=3;
 	public static void main(String[] args) {
 		AssignCountries temp= new AssignCountries();
 		temp.assignCountries();
 		NumberOfPlayers=temp.getNumberOfPlayer();
 	}
+	public static int NumberOfPlayers=2;
+
 	/**
 	 * returns the number of player chooses as integer
 	 * @return int
@@ -29,19 +29,19 @@ public class AssignCountries {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				//jumps to new window here
 				 NumberOfPlayers = Integer.parseInt((String) PNumberJCombo.getSelectedItem());	
 				 System.out.print("returns" + NumberOfPlayers);
 				 Frame1.dispose();
-				 MainControll mc;
+				
 				try {
-					mc = new MainControll();
-					mc.Function();
-				} catch (IOException e1) {
+					 MainControll controll = new MainControll();
+					controll.Function();
+				} catch (Exception e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				 
+					
+					
 			} 
 			
 		});
@@ -58,6 +58,7 @@ public class AssignCountries {
 		OKButton.setBounds(290,190,100,30);
 		
 String[] select= {"2", "3", "4","5", "6"};
+
 		PNumberJCombo= new JComboBox(select);
 		PNumberJCombo.setBounds(120,150,200,20);
 	   	
@@ -71,6 +72,7 @@ String[] select= {"2", "3", "4","5", "6"};
 	   	Label1.setVisible(true);
 	   	Frame1.setLayout(null);
 	   	Frame1.setVisible(true);
+	   	NumberOfPlayers=getNumberOfPlayer();
 	}
 	
 	

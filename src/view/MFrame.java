20b4ebefa6a6513dde09bf7d2ba1d38a5e7cb7ec
neerajpreetkaur <45363591+neerajpreetkaur@users.noreply.github.com>
2019-03-05 18:39:48 -0600

    Update MFrame.java

--- src/view/MFrame.java
+++ src/view/MFrame.java
@@ -1,4 +1,5 @@
 package view;
+
 import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Color;
@@ -14,7 +15,6 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-//import java.lang.ModuleLayer.Controller;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -34,7 +34,11 @@ import javax.swing.JScrollPane;
 
 import javafx.scene.layout.Border;
 import model.Country;
-
+/**
+ * This class represents user interface for the game play
+ * @author pazim
+ * @version 1.0
+ */
 public class MFrame extends JFrame {
 	private JButton[] button;
 	JPanel jPanel2;
@@ -49,34 +53,31 @@ public class MFrame extends JFrame {
 	String CCC;
 	public String MapImage = "noimage.bmp";
 
-	/*
-	 * ReadingFiles files; Reinforcement reinforcement; MainControll controll;
-	 */
 	public MFrame(MyActionListner myActionListner, String MapImage) {
-		// TODO Auto-generated constructor stub
+
 		super("PAzim");
 		this.myActionListner = myActionListner;
 		this.MapImage = MapImage;
 	}
-
-	public void fun() throws Exception  {
-		/*
-		 * files = new ReadingFiles(); reinforcement = new Reinforcement();
-		 * files.Reads(); controll = new MainControll();
-		 */ hashButton = new HashMap<>();
+	/**
+	 * This method displays the user interface
+	 * @throws Exception
+	 */
+	public void fun() throws Exception {
+		hashButton = new HashMap<>();
 		FlowLayout flowLayout = new FlowLayout();
 		BorderLayout borderLayout = new BorderLayout();
 		JPanel mainPanel = new JPanel(new GridLayout(1, 2));
-		// Pannel 1
+
 		JPanel jPanel = new JPanel(new GridLayout(2, 1));
 		jPanel.setSize(new Dimension(500, 1000));
 		jPanel.setBackground(Color.red);
 		BufferedImage image;
 		try {
-		 image = ImageIO.read(new File("Resources/" + MapImage));
-		}catch(Exception e) {
-			 image = ImageIO.read(new File("Resources/noimage.bmp"));
-			
+			image = ImageIO.read(new File("Resources/" + MapImage));
+		} catch (Exception e) {
+			image = ImageIO.read(new File("Resources/noimage.bmp"));
+
 		}
 		JLabel label = new JLabel(new ImageIcon(image));
 		JScrollPane scroller = new JScrollPane(label, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
@@ -91,7 +92,7 @@ public class MFrame extends JFrame {
 		nextAction.addActionListener(myActionListner);
 		jPanel5.add(nextAction);
 		jLabel = new JLabel("Turn For:- " + playerTurn);
-		// jLabel.setSize(MAXIMIZED_HORIZ, MAXIMIZED_VERT);
+	
 		jLabel2 = new JLabel("Armies Left:- " + noArmiesLeft);
 		jLabel3 = new JLabel("Neighbours :- " + CCC);
 		jLabel4 = new JLabel("HIIIIIII:- " + AAA);
@@ -106,7 +107,7 @@ public class MFrame extends JFrame {
 		jPanel4.add(jLabel4);
 		jPanel4.add(jLabel5);
 		jPanel.add(jPanel3);
-		// Pannel 2
+
 		jPanel2 = new JPanel(new GridLayout(0, 5));
 		jPanel2.setBackground(Color.BLACK);
 		jPanel2.setSize(new Dimension(500, 1000));
@@ -122,7 +123,9 @@ public class MFrame extends JFrame {
 		setVisible(true);
 
 	}
-
+	/**
+	 * This method update the view
+	 */
 	public void NotifyAll() {
 		jLabel.setText("Turn For Player :-" + (myActionListner.currentPlayer + 1));
 		jLabel2.setText("Armies Left:- " + noArmiesLeft);
@@ -130,7 +133,11 @@ public class MFrame extends JFrame {
 		jLabel4.setText("DICE 1:- " + AAA);
 		jLabel5.setText("DICE 2:- " + BBB);
 	}
-
+	/**
+	 * This method assigns button to each country in UI
+	 * @param countryObjects
+	 * @throws IOException
+	 */
 	public void SetButtons(HashMap<String, Country> countryObjects) throws IOException {
 		List<String> count = new ArrayList<>(countryObjects.keySet());
 		button = new JButton[count.size()];
@@ -139,26 +146,17 @@ public class MFrame extends JFrame {
 		for (int i = 0; i < count.size(); i++) {
 			button[i] = new JButton(count.get(i) + " | " + countryObjects.get(count.get(i)).getNoOfArmies() + " | "
 					+ countryObjects.get(count.get(i)).getContinent().getName());
-			// button[i].setBackground(arrayList.get(random.nextInt(4)));
+
 			hashButton.put((String) count.get(i), button[i]);
 			jPanel2.add(button[i]);
-			/*
-			 * JButton butto= new JButton("1"); butto.setEnabled(false);
-			 * butto.setBackground(Color.RED);
-			 * 
-			 */
+
 			button[i].addActionListener(myActionListner);
 		}
 
 	}
-
-	/*
-	 * public void SetColorToAll(List<Country> countries, Player player) { for (int
-	 * i = 0; i < countries.size(); i++) {
-	 * hashButton.get(countries.get(i).getName()).setBackground(player.
-	 * getPlayerColor()); }
-	 * 
-	 * }
+	/**
+	 * This method give color to the countries
+	 * @param countryObjects
 	 */
 	public void SetColorToAll(HashMap<String, Country> countryObjects) {
 		List<String> count = new ArrayList<>(countryObjects.keySet());
@@ -167,6 +165,11 @@ public class MFrame extends JFrame {
 		}
 
 	}
+	/**
+	 * This method runs after every button click
+	 * @param countryObjects
+	 * @throws IOException
+	 */
 
 	public void Refresh(HashMap<String, Country> countryObjects) throws IOException {
 		List<String> count = new ArrayList<>(countryObjects.keySet());
@@ -193,7 +196,7 @@ public class MFrame extends JFrame {
 	public void ActivateAll() {
 		for (int i = 0; i < button.length; i++) {
 			button[i].setEnabled(true);
-		
+
 		}
 	}
 
@@ -209,7 +212,6 @@ public class MFrame extends JFrame {
 		for (int i = 0; i < temp.size(); i++) {
 			JButton tempb = hashButton.get(temp.get(i));
 			tempb.setEnabled(false);
-			// tempb.setContentAreaFilled(false);
 
 		}
 

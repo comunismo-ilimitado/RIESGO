cb06a029e32f47e5ab3f6896cf6555ba615c330b
Navjot08 <navjotkb08@gmail.com>
2019-03-05 20:45:17 -0500

    Test suite class

--- test/TestSuite.java
+++ test/TestSuite.java
@@ -1,4 +1,19 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 
+@RunWith(Suite.class)
+@SuiteClasses({FortificationTest.class,
+	MapValidationTest.class,
+	ReinforcementTest.class
+	
+})
+
+/**
+ * This class runs all the test classes
+ * @author Navjot kaur
+ *
+ */
 public class TestSuite {
 
 }

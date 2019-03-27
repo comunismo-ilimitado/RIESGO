eeef5f4347a8d87befdd2459aa7c6097181767d3
neerajpreetkaur <45363591+neerajpreetkaur@users.noreply.github.com>
2019-03-27 16:22:38 -0500

    Update StartupPhaseTest.java

--- test/ControllerTests/StartupPhaseTest.java
+++ test/ControllerTests/StartupPhaseTest.java
@@ -11,25 +11,42 @@ import org.junit.Test;
 import controller.ReadingFiles;
 import view.MFrame2;
 
+
+/**
+*Tests startup phase 
+*@author navjot kaur
+*/
+
 public class StartupPhaseTest 
 {
 	MFrame2 mf = new MFrame2();
 	ReadingFiles rf = new ReadingFiles(mf);
 	String str = "Resources/Montreal.map";
 	int temp;
-	
+	/**
+	* method called before all test
+	*
+	*/
 	@Before
 	public void onStart()
 	{
 		temp = ReadingFiles.ArmiesPerPlayer;
 	}
 	
+	/**
+	* method called after all test
+	*
+	*/
 	@After
 	public void onEnd()
 	{
 		ReadingFiles.ArmiesPerPlayer = temp;
 	}
 	
+	/**
+	*Tests armies of the player
+	*
+	*/
 	@Test
 	public void testArmiesPerPlayer() throws IOException
 	{

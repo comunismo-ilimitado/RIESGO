c0633e6f499d878c919bd5923b77889c62e79f2d
ggronda <jesusggironda01@gmail.com>
2022-04-20 11:56:45 +0200

    Revisión de AgressiveStrategy.

--- src/controller/strategies/AggressiveStrategy.java
+++ src/controller/strategies/AggressiveStrategy.java
@@ -25,10 +25,10 @@ public class AggressiveStrategy extends Strategy {
         exchangeCardsStrategy(player);
         player.calcArmiesByControlValue();
         List<Country> countries = player.getMyCountries(player);
-        Country strongestcountry = null;
-        if (player.getMyCountries(player).size() > getStrongestCountry(countries))
-            strongestcountry = player.getMyCountries(player).get(getStrongestCountry(countries));
-        strongestcountry.setNoOfArmies(strongestcountry.getNoOfArmies() + player.getPlayerArmiesNotDeployed());
+
+        //if (player.getMyCountries(player).size() > getStrongestCountry(countries))
+         Country  strongestcountry = player.getMyCountries(player).get(getStrongestCountry(countries));
+        strongestcountry.setNoOfArmies(strongestcountry.getNoOfArmies() + player.getPlayerArmiesNotDeployed()); // nºarmies = nºarmies + all reinforcements
         player.getMyCountries(player).get(getStrongestCountry(countries))
                 .setNoOfArmies(strongestcountry.getNoOfArmies());
         ReadingFiles.getCountryNameObject().get(strongestcountry.getName()).setNoOfArmies(strongestcountry.getNoOfArmies());
@@ -66,6 +66,7 @@ public class AggressiveStrategy extends Strategy {
         FortificationController fC = new FortificationController();
         List<Country> countries = player.getMyCountries(player);
         Country strongestcountry = countries.get(getStrongestCountry(countries));
+        player.setTotalCountriesOccupied(countries);
         countries.remove(strongestcountry);
         while (countries.size() > 0) {
             Country fotifyingcountry = countries.get(getStrongestCountry(countries));
@@ -73,11 +74,6 @@ public class AggressiveStrategy extends Strategy {
             if (fotifyingcountry.getNoOfArmies() > 1 && fC.hasPathBFS2(fotifyingcountry, strongestcountry)) {
                 strongestcountry.setNoOfArmies(strongestcountry.getNoOfArmies() + fotifyingcountry.getNoOfArmies() - 1);
                 fotifyingcountry.setNoOfArmies(1);
-                countries.add(strongestcountry);
-                countries.add(fotifyingcountry);
-                player.setTotalCountriesOccupied(countries);
-                countries.remove(strongestcountry);
-                countries.remove(fotifyingcountry);
                 ReadingFiles.getCountryNameObject().get(fotifyingcountry.getName())
                         .setNoOfArmies(fotifyingcountry.getNoOfArmies());
                 ReadingFiles.getCountryNameObject().get(strongestcountry.getName())

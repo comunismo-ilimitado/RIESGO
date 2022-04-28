d4c02d375ee9eb72c51fd1e852aae2843e3214eb
AlejandroAdrados <alejandroadrados@gmail.com>
2022-04-28 20:21:18 +0200

    Avances en JavaFX

--- src/TestingUI/HelloController.java
+++ src/TestingUI/HelloController.java
@@ -2,13 +2,32 @@ package TestingUI;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void exitGame() {
+        System.exit(0);
+    }
+
+    public void changeColorEnter2(MouseEvent mouseEvent) {
+        Button button = (Button) mouseEvent.getSource();
+        button.setStyle("-fx-background-color: #1555bf");
+    }
+
+    public void changeColorOut2(MouseEvent mouseEvent) {
+        Button button = (Button) mouseEvent.getSource();
+        button.setStyle("-fx-background-color: #6d99d5");
+    }
+
+    public void play(MouseEvent mouseEvent) {
+        System.out.println("Juego");
+    }
+
+    public void settings(MouseEvent mouseEvent) {
+        System.out.println("Ajustes");
     }
 }
--- src/TestingUI/HelloFX.java
+++ src/TestingUI/HelloFX.java
@@ -13,6 +13,8 @@ public class HelloFX extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloFX.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
+        stage.setFullScreen(false);
+        stage.setResizable(true);
         stage.setScene(scene);
         stage.show();
     }
